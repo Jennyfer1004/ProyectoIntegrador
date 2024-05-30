5048f19f3c94773f574c26028f5ce51a94cebf79
DanielFernandoMartinezArias <dmartinez33@udi.edu.co>
2024-05-29 19:01:23 -0500

    ventana consultar vendedores funcional

--- ProyectoIntegrador/src/logica/controladores/vendedor/ConsultarVendedoresController.java
+++ ProyectoIntegrador/src/logica/controladores/vendedor/ConsultarVendedoresController.java
@@ -1,44 +1,77 @@
 package logica.controladores.vendedor;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import datos.objetos.Proveedor;
+import datos.objetos.Vendedor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import logica.VendedorService;
 import presentacion.vistas.gerente.GerenteView;
 import javafx.scene.control.TableView;
-
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.control.TableColumn;
 
-public class ConsultarVendedoresController {
+public class ConsultarVendedoresController implements Initializable{
 	@FXML
-	private TableView tabla;
+	private TableView <Vendedor>tabla;
 	@FXML
-	private TableColumn cedulaColumna;
+	private TableColumn <Vendedor,String> cedulaColumna;
 	@FXML
-	private TableColumn nombreCompletoColumna;
+	private TableColumn <Vendedor,String> nombreCompletoColumna;
 	@FXML
-	private TableColumn correoColumna;
+	private TableColumn <Vendedor,String> correoColumna;
 	@FXML
-	private TableColumn telefonoColumna;
+	private TableColumn <Vendedor,String> telefonoColumna;
 	@FXML
-	private TableColumn direccionColumna;
+	private TableColumn <Vendedor,String> direccionColumna;
+	@FXML
+	private TableColumn <Vendedor,String> estadoColumna;
 	@FXML
 	private Button botonConsultarVendedores;
 	@FXML
-	private Button botonVolver;
-
+	private Button botonVolvera;
+	private VendedorService vendedorService;
+	private  ObservableList<Vendedor> listaVendedores;
 	
 	GerenteView gv = new GerenteView();
 	@FXML
 	public void onConsultarVendedoresClicked(MouseEvent event) {
+		this.tabla.getItems().clear();
+        this.tabla.setItems(this.vendedorService.getVendedores());
 	}
 	
 	@FXML
 	public void onVolverClicked(MouseEvent event) throws IOException {
-		gv.show((Stage) botonVolver.getScene().getWindow());
+		System.out.println("volver");
+		gv.show((Stage) botonVolvera.getScene().getWindow());
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.vendedorService = new VendedorService();
+        this.listaVendedores =  FXCollections.observableArrayList(); 
+        
+        nombreCompletoColumna.setCellFactory(TextFieldTableCell.forTableColumn());
+        direccionColumna.setCellFactory(TextFieldTableCell.forTableColumn());
+        correoColumna.setCellFactory(TextFieldTableCell.forTableColumn());
+        telefonoColumna.setCellFactory(TextFieldTableCell.forTableColumn());
+        estadoColumna.setCellFactory(TextFieldTableCell.forTableColumn());
+		
+        cedulaColumna.setCellValueFactory(new PropertyValueFactory<>("cedula"));
+        nombreCompletoColumna.setCellValueFactory(new PropertyValueFactory<>("nombreCompleto"));
+        direccionColumna.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+        correoColumna.setCellValueFactory(new PropertyValueFactory<>("correo"));
+        telefonoColumna.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        estadoColumna.setCellValueFactory(new PropertyValueFactory<>("estado"));
 	}
 }
